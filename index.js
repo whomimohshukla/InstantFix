55ebc7e4a745b209d77b5f06961e9073ac3d1ffb
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-06-03 00:57:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
