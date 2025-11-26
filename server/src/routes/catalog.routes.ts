31abe16607def935f451d9b49658663ab9e553ae
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-11-27 03:41:57 +0530

    bookings enhacnement

--- server/src/routes/catalog.routes.ts
+++ server/src/routes/catalog.routes.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import {
   categoriesCtrl,
   servicesCtrl,
+  serviceDetailCtrl,
 } from "../controllers/Catalog.controllers";
 
 export const catalogRouter = Router();
 
 catalogRouter.get("/categories", categoriesCtrl);
 catalogRouter.get("/services", servicesCtrl);
+catalogRouter.get("/services/:id", serviceDetailCtrl);
