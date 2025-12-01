43bce55287b6e3a0666b3e1cf55ae3c113c167a5
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-12-02 00:00:23 +0530

    Refactor code for consistency and readability across multiple controller and configuration files

--- server/src/config/mailer.ts
+++ server/src/config/mailer.ts
@@ -6,20 +6,22 @@ dotenv.config();
 // Ensure environment variables are set
 const { MAIL_HOST, MAIL_USER, MAIL_PASS } = process.env;
 if (!MAIL_HOST || !MAIL_USER || !MAIL_PASS) {
-    console.warn("Mail configuration is incomplete.");
+	console.warn("Mail configuration is incomplete.");
 }
 
-const transporter = (MAIL_HOST && MAIL_USER && MAIL_PASS)
-  ? nodemailer.createTransport({
-      host: MAIL_HOST,
-      port: 587,
-      secure: false,
-      auth: {
-        user: MAIL_USER,
-        pass: MAIL_PASS,
-      },
-    })
-  : nodemailer.createTransport({ jsonTransport: true });
+const transporter =
+	MAIL_HOST && MAIL_USER && MAIL_PASS
+		? nodemailer.createTransport({
+				host: MAIL_HOST,
+				port: 587,
+				secure: false,
+				auth: {
+					user: MAIL_USER,
+					pass: MAIL_PASS,
+				},
+		  })
+		: nodemailer.createTransport({ jsonTransport: true });
 
-export const mailFrom = process.env.MAIL_FROM || "InstantFix <no-reply@instantfix.com>";
+export const mailFrom =
+	process.env.MAIL_FROM || "InstantFix <no-reply@instantfix.com>";
 export default transporter;
--- server/src/config/plivo.ts
+++ server/src/config/plivo.ts
@@ -1,12 +1,11 @@
 import * as plivo from "plivo";
 
-
 // Load Plivo configuration from environment variables
 const { PLIVO_AUTH_ID, PLIVO_AUTH_TOKEN, PLIVO_SRC_NUMBER } = process.env;
 
 export const plivoClient = new plivo.Client(
-  PLIVO_AUTH_ID || "",
-  PLIVO_AUTH_TOKEN || ""
+	PLIVO_AUTH_ID || "",
+	PLIVO_AUTH_TOKEN || ""
 );
 
 export const plivoFrom = PLIVO_SRC_NUMBER || "INSTANTFIX";
