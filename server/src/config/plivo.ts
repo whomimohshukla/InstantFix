e3af0af37151fb2334be9f5ea588950267dc70f5
Mimohshukla00 <mimohshukla0001@gmail.com>
2025-12-01 23:58:38 +0530

    Add comment to clarify Plivo configuration loading

--- server/src/config/plivo.ts
+++ server/src/config/plivo.ts
@@ -1,5 +1,7 @@
 import * as plivo from "plivo";
 
+
+// Load Plivo configuration from environment variables
 const { PLIVO_AUTH_ID, PLIVO_AUTH_TOKEN, PLIVO_SRC_NUMBER } = process.env;
 
 export const plivoClient = new plivo.Client(
